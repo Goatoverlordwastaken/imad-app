f80fe18827cd1380145b0e0a3aaad6aa47742d96
Goatoverlordwastaken <30385238+Goatoverlordwastaken@users.noreply.github.com>
2017-08-23 12:53:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'home-header.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'header-home.html'));
 });
 
 app.get('/article-two', function (req, res) {
