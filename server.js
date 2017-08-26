2f4343615c601b540d85665e87d806c0b091a8b2
Goatoverlordwastaken <30385238+Goatoverlordwastaken@users.noreply.github.com>
2017-08-26 22:22:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("The secod article will be provided here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("The third article will be provided here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
