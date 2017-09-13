c4d1559f8556dc25de019f81cc4dd16246b7399d
Goatoverlordwastaken <30385238+Goatoverlordwastaken@users.noreply.github.com>
2017-09-13 19:14:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = 'New Value';
+
+//move the image
+
+var element = document.getElementById("img");
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};
