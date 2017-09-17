13f615b266f0fa2b5f4251af5237694454016ebb
Goatoverlordwastaken <30385238+Goatoverlordwastaken@users.noreply.github.com>
2017-09-17 19:41:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,5 @@
-/*console.log('Loaded!');
+console.log('Loaded!');
 
-//changing text in the index 
-
-var element = document.getElementById("main-text");
-
-element.innerHTML = 'New Value';
-
-//move the image
-
-var img = document.getElementById("madi");
-var marginLeft =0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-}; */
-console.log("JS Loaded")
 var button = document.getElementById('counter');
 var counter = 0;
 button.onClick = function () {
