0166033c5da0bd6d1249602d37a500ab286f57af
Goatoverlordwastaken <30385238+Goatoverlordwastaken@users.noreply.github.com>
2017-09-17 19:09:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,27 @@ function moveRight() {
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 };
+
+
+
+var button = document.getElementByID("counter");
+button.onClick = function () {
+    //making request object
+    var request = XMLHttpRequest();
+    // capture response 
+    request.onReadyStateChange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // take action
+            if (request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+            }
+        }
+        //
+    };
+    
+    //make the request
+    request.open('GET', 'http://pveleneni.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
